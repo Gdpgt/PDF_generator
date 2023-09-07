4476ae0ef6a7d481ef10b9b02a7ea1a1d934b9b1
Guillaume de Puget <guillaumedepuget@gmail.com>
2023-09-07 14:46:32 +0200

    Day 24 : add footers

--- main.py
+++ main.py
@@ -4,15 +4,30 @@ import pandas as pd
 df = pd.read_csv("topics.csv")
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)  # Essential for the footer to be correctly placed
 
 for index, row in df.iterrows():
     pdf.add_page()
+
+    # Header :
     pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(100, 100, 100) # RGB for gray. Goes from 0 to 254
+    pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align="L", ln=1, border=0)
     pdf.line(10, 21, 200, 21)
 
+    # Footer :
+    pdf.ln(265)  # The total A4 page is 297m long
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row['Topic'], align="R")
+
     for i in range(row['Pages'] - 1):
         pdf.add_page()
+
+        # Footer :
+        pdf.ln(277)  # longer than the previous footer because there are no header
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row['Topic'], align="R")
 
 pdf.output("output.pdf")
